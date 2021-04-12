829ab85e8d51ac86e8f75aff28d1cc3d18391f14
Solovyev Aleksey <solovyev.a@icloud.com>
2021-04-12 08:29:56 +0300

    Load nunjucks global functions and filters

--- config/loaders/nunjucks.loader.js
+++ config/loaders/nunjucks.loader.js
@@ -1,3 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+const { SOURCE_DIR } = require('../helpers')
+
+
+/* Get nunjucks globals */
+const getNunujucksGlobals = type => {
+  const fullPath = path.resolve(SOURCE_DIR, 'njk', 'globals', type)
+  const data = {  }
+
+  fs.readdirSync(fullPath).forEach(fileName => {
+    if (!/\.js$/i.test(fileName)) return false
+
+    data[fileName.split('.')[0]] = path.join(fullPath, fileName)
+  })
+
+  return data
+}
+
+
 /**
  * Loads nunjucks files.
  * https://github.com/ogonkov/nunjucks-loader
@@ -8,7 +28,13 @@ module.exports = {
     loader: 'simple-nunjucks-loader',
     options: {
       searchPaths: ['src/njk'],
-      assetsPaths: ['src/images']
+      assetsPaths: ['src/images'],
+      globals: {
+        ...getNunujucksGlobals('functions')
+      },
+      filters: {
+        ...getNunujucksGlobals('filters')
+      }
     }
   }
 }
