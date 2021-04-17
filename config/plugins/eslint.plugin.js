c27e7be368fedff85817efc422e5ffe7233eda39
Solovyev Aleksey <solovyev.a@icloud.com>
2021-04-17 12:00:43 +0300

    Enable Eslint for TypeScript files
    ## Installed dependencies:
    - @typescript-eslint/eslint-plugin 4.22.0
    - @@typescript-eslint/parser 4.22.0

--- config/plugins/eslint.plugin.js
+++ config/plugins/eslint.plugin.js
@@ -6,5 +6,6 @@ const ESLintPlugin = require('eslint-webpack-plugin')
  * https://github.com/webpack-contrib/eslint-webpack-plugin
  */
 module.exports = new ESLintPlugin({
+  extensions: ['js', 'ts'],
   baseConfig: require('../.eslintrc')
 })
